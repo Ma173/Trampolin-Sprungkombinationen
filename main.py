686003e3d39645b55ce49db5b2be58acb4dc96b5
Ma173 <malte.gerhardt@rub.de>
2020-01-30 04:00:18 +0000

    In den Werten der Schlüssel des Dicts elemente aus Strings Tuple gemacht: erstes Glied sind die machbaren Sprünge und zweites Glied ist die aus den Sprüngen resultierende Position

--- main.py
+++ main.py
@@ -2,21 +2,21 @@ import random
 
 elemente = {
     "Stand": [
-        "Bauch", "halbe Bauch", "Sitz", "halbe Sitz", "Rücken", "halbe Rücken",
-        "ganze Rücken", "halbe Schraube", "ganze Schraube", "Grätsche",
-        "Hocke", "Bücke", "halbe Grätsche", "halbe Hocke",
-        "Hocke in den halben Sitz"
+        ("Bauch","Bauch", ("halbe Bauch","Bauch"), ("Sitz","Sitz"), ("halbe Sitz","Sitz"), ("Rücken","Rücken"), ("halbe Rücken":"Rücken"),
+        ("ganze Rücken","Rücken"), ("halbe Schraube","Stand"), ("ganze Schraube":"Stand"), ("Grätsche","Stand")
+        ("Hocke","Stand"), ("Bücke", "Stand"),("halbe Grätsche","Stand"), ("halbe Hocke","Stand)",
+        ("Hocke in den halben Sitz","Sitz")
     ],
     "Rücken": [
-        "Stand", "habe Stand", "ganze Stand", "halbe Rücken", "Bauch",
-        "halbe Bauch", "Sitz", "halbe Sitz", "Muffel", "Muffel in den Sitz",
-        "Muffel in den Bauch", "Muffel in den Rücken","Wende in den Bauch"
+        ("Stand","Stand", ("habe Stand","Stand"), ("ganze Stand","Stand"), ("halbe Rücken","Rücken"), ("Bauch","Bauch"),
+        ("halbe Bauch","Bauch"),("Sitz","Sitz"), ("halbe Sitz","Sitz"), ("Muffel","Stand"), ("Muffel in den Sitz","Sitz"),
+        ("Muffel in den Bauch","Bauch"), ("Muffel in den Rücken","Rücken"),("Wende in den Bauch","Bauch")
     ],
     "Sitz": [
-        "Stand", "halbe Stand", "ganze Stand", "Sitz", "Bauch", "halbe Bauch",
-        "Rücken", "halbe Rücken"
+        ("Stand","Stand"), ("halbe Stand","Stand"), ("ganze Stand","Stand"), ("Sitz","Sitz"), ("Bauch", "Bauch"),("halbe Bauch","Bauch"),
+        ("Rücken","Rücken", ("halbe Rücken","Rücken")
     ],
-    "Bauch": ["Stand","halbe Stand","Rücken","halbe Heli","Bauch","Wende in den Rücken"],
+    "Bauch": [("Stand","Stand"),("halbe Stand","Stand"),("Rücken","Rücken"),("halbe Heli","Bauch"),("Bauch","Bauch"),("Wende in den Rücken","Rücken"],
     
 }
 
