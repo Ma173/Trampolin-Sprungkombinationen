ae391c383ac88f852dcccb251109fc1335c75b81
Ma173 <malte.gerhardt@rub.de>
2020-01-31 02:13:04 +0000

    Erweiterung des Codes durch ehemaligen Code aus Experimental

--- main.py
+++ main.py
@@ -2,41 +2,36 @@ import random
 
 elemente = {
     "Stand": [
-        "Bauch", "halbe Bauch", "Sitz", "halbe Sitz", "Rücken", "halbe Rücken",
-        "ganze Rücken", "halbe Schraube", "ganze Schraube", "Grätsche",
-        "Hocke", "Bücke", "halbe Grätsche", "halbe Hocke",
-        "Hocke in den halben Sitz"
+        ("Bauch","Bauch"), ("halbe Bauch","Bauch"), ("Sitz","Sitz"), ("halbe Sitz","Sitz"), ("Rücken","Rücken"), ("halbe Rücken","Rücken"),
+        ("ganze Rücken","Rücken"), ("halbe Schraube","Stand"), ("ganze Schraube","Stand"), ("Grätsche","Stand"),
+        ("Hocke","Stand"), ("Bücke", "Stand"),("halbe Grätsche","Stand"), ("halbe Hocke","Stand"),
+        ("Hocke in den halben Sitz","Sitz")
     ],
     "Rücken": [
-        "Stand", "habe Stand", "ganze Stand", "halbe Rücken", "Bauch",
-        "halbe Bauch", "Sitz", "halbe Sitz", "Muffel", "Muffel in den Sitz",
-        "Muffel in den Bauch", "Muffel in den Rücken","Wende in den Bauch"
+        ("Stand","Stand"), ("halbe Stand","Stand"), ("ganze Stand","Stand"), ("halbe Rücken","Rücken"), ("Bauch","Bauch"),
+        ("halbe Bauch","Bauch"),("Sitz","Sitz"), ("halbe Sitz","Sitz"), ("Muffel","Stand"), ("Muffel in den Sitz","Sitz"),
+        ("Muffel in den Bauch","Bauch"), ("Muffel in den Rücken","Rücken"),("Wende in den Bauch","Bauch")
     ],
     "Sitz": [
-        "Stand", "halbe Stand", "ganze Stand", "Sitz", "Bauch", "halbe Bauch",
-        "Rücken", "halbe Rücken"
+        ("Stand","Stand"), ("halbe Stand","Stand"), ("ganze Stand","Stand"), ("Sitz","Sitz"), ("Bauch", "Bauch"),("halbe Bauch","Bauch"),
+        ("Rücken","Rücken"), ("halbe Rücken","Rücken")
     ],
-    "Bauch": ["Stand","halbe Stand","Rücken","halbe Heli","Bauch","Wende in den Rücken"],
-    ""
+    "Bauch": [("Stand","Stand"),("halbe Stand","Stand"),("Rücken","Rücken"),("halbe Heli","Bauch"),("Bauch","Bauch"),("Wende in den Rücken","Rücken")],
+    
 }
-
 
 uebung = []
 spruengeInUebung = 0
 while spruengeInUebung < 10:
 	if spruengeInUebung == 0:
 	  #Beginn im Stand
-		erstesElement = elemente["Stand"][random.randrange(0, len(elemente))]
-		uebung.append(erstesElement)
+		erstesElementMitFolgeelement = elemente["Stand"][random.randrange(0, len(elemente))]
+		uebung.append(erstesElementMitFolgeelement)
 	elif spruengeInUebung > 0:
-		letztesElement=uebung[-1]
-		erlaubtesElement="nein"
-		while erlaubtesElement=="nein":
-		  zufallselement= list(elemente.keys())[random.randrange(0,len(elemente))]
-		  if letztesElement in elemente[zufallselement]:
-		    erlaubtesElement="ja"
+		letztePosition=uebung[-1][1]
+		zufallselement= elemente[letztePosition][random.randrange(0,len(elemente))]
 		uebung.append(zufallselement)
 	spruengeInUebung += 1
 print("\n\n")
 for sprung in uebung:
-  print (sprung)
+  print (sprung[0])
