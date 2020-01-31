48538bc675f0164e881772b9a6402652c07377a1
Ma173 <malte.gerhardt@rub.de>
2020-01-31 02:11:57 +0000

    Gruppierung nach Ziel-Position

--- main.py
+++ main.py
@@ -1,22 +1,78 @@
 import random
+elemente={
 
+  #TODO: Elemente sollten über die Nummer im Dict sperrbar sein
+  #TODO: Kombinationen erfragen und falls machbar in jeweiligem Tupel nachtragen:
+  # Geht Bauch - halbe Sitz?
+  # Geht Bauch - Ganze Stand?
+  # Geht Bücke in den Sitz?
+  # Wie viele Punkte gibt Sitz ganze Sitz? 0.2?
+  
+#INFO: Muffel zählt offiziell als Salto; Rücken vorwärtsrolle zählt auch als Salto oder Porpoise (Tümmler);
+
+
+  #Elementname : (Schwierigkeit, Position vorher, Position nachher,Weirdheitsfaktor 0 (gewöhnlich)- 1 (ungewöhnlich) - 2 (schwer machbar/ sehr ungewohnt))
+
+  #IN STAND
+  "Stand":((0.1,"Rücken","Stand",0),(0.1,"Bauch","Stand",0),(0,"Sitz","Stand",0))
+  "Hocke":((0,"Stand","Stand",0),
+  "Bücke":(0,"Stand","Stand",0),
+  "Grätsche":(0,"Stand","Stand",0),
+  "halbe Schraube":(0.1,"Stand","Stand",0),
+  "halbe Hocke":(0.1,"Stand","Stand",1),
+  "halbe Bücke":(0.1,"Stand","Stand",1),
+  "halbe Grätsche":(0.1,"Stand","Stand",1),
+  "ganze Schraube":(0.2,"Stand","Stand",0),
+  "ganze Stand":((0.2,"Sitz","Stand",1),(0.3,"Rücken","Stand",1)),
+  "Muffel":(0.3,"Rücken","Stand",0),
+  "Salto vorwärts z. Stand":(0.6,"Rücken","Rücken",1),
+
+  #IN SITZ
+  "Sitz":((0,"Stand","Stand",0),(0,"Sitz","Sitz")),
+  "halbe Sitz":((0.1,"Stand","Sitz",0),(0.1,"Sitz", "Sitz",0),(0.2,"Rücken","Sitz",1))
+  "ganze Sitz":((0.2,"Sitz","Sitz",0),(0.2,"Stand","Sitz",1)),
+  "Hocke in den Sitz":(0,"Stand","Sitz",2),
+  "Grätsche in den Sitz":(0,"Stand","Sitz",2),
+  "Muffel in den Sitz":(0.3,"Rücken","Stand",0),
+  
+  #IN RÜCKEN
+  "Rücken":((0.1,"Stand","Rücken",0),(0.1,"Sitz","Rücken",1),(0,"Rücken","Rücken",0),(0.2,"Bauch","Rücken",0))
+  "Halbe Rücken":((0.2,"Stand","Rücken",0),(0.3,"Rücken","Rücken",0),(0.2,"Sitz","Halbe Rücken",1)),
+  "Salto vorwärts z. Rücken c":(0.5,"Rücken","Rücken"),
+  "Muffel in den Rücken":(0.5,"Rücken","Stand",1),
+  
+  #IN BAUCH
+  "Bauch":((0.1,"Stand","Bauch",0),(0.1,"Sitz","Bauch",0),(0,"Bauch","Bauch",0),(0.2,"Rücken","Bauch",1)),
+  "halbe Heli":(0.1,"Bauch","Bauch",0),
+  "Muffel in den Bauch":(0.2,"Rücken","Bauch",1)
+
+  ""
+
+}
+
+gesperrteElemente=[]
+users={
+  #Name: [gesperrte Elemente (List) ,weirde Sprünge (Int, 0 (keine) - 2 (alle))]
+  "Malte":[[13.2],1]
+}
 elemente = {
     "Stand": [
-        "Bauch", "halbe Bauch", "Sitz", "halbe Sitz", "Rücken", "halbe Rücken",
-        "ganze Rücken", "halbe Schraube", "ganze Schraube", "Grätsche",
-        "Hocke", "Bücke", "halbe Grätsche", "halbe Hocke",
-        "Hocke in den halben Sitz"
+        ("Bauch","Bauch"), ("halbe Bauch","Bauch"), ("Sitz","Sitz"), ("halbe Sitz","Sitz"), ("Rücken","Rücken"), ("halbe Rücken","Rücken"),
+        ("ganze Rücken","Rücken"), ("halbe Schraube","Stand"), ("ganze Schraube","Stand"), ("Grätsche","Stand"),
+        ("Hocke","Stand"), ("Bücke", "Stand"),("halbe Grätsche","Stand"), ("halbe Hocke","Stand"),
+        ("Hocke in den halben Sitz","Sitz")
     ],
     "Rücken": [
-        "Stand", "habe Stand", "ganze Stand", "halbe Rücken", "Bauch",
-        "halbe Bauch", "Sitz", "halbe Sitz", "Muffel", "Muffel in den Sitz",
-        "Muffel in den Bauch", "Muffel in den Rücken","Wende in den Bauch"
+        ("Stand","Stand"), ("halbe Stand","Stand"), ("ganze Stand","Stand"), ("halbe Rücken","Rücken"), ("Bauch","Bauch"),
+        ("halbe Bauch","Bauch"),("Sitz","Sitz"), ("halbe Sitz","Sitz"), ("Muffel","Stand"), ("Muffel in den Sitz","Sitz"),
+        ("Muffel in den Bauch","Bauch"), ("Muffel in den Rücken","Rücken"),("Wende in den Bauch","Bauch")
     ],
     "Sitz": [
-        "Stand", "halbe Stand", "ganze Stand", "Sitz", "Bauch", "halbe Bauch",
-        "Rücken", "halbe Rücken"
+        ("Stand","Stand"), ("halbe Stand","Stand"), ("ganze Stand","Stand"), ("Sitz","Sitz"), ("Bauch", "Bauch"),("halbe Bauch","Bauch"),
+        ("Rücken","Rücken"), ("halbe Rücken","Rücken")
     ],
-    "Bauch": ["Stand","halbe Stand","Rücken","halbe Heli","Bauch","Wende in den Rücken"],
+    "Bauch": [("Stand","Stand"),("halbe Stand","Stand"),("Rücken","Rücken"),("halbe Heli","Bauch"),("Bauch","Bauch"),("Wende in den Rücken","Rücken")],
+    
 }
 
 uebung = []
@@ -24,17 +80,13 @@ spruengeInUebung = 0
 while spruengeInUebung < 10:
 	if spruengeInUebung == 0:
 	  #Beginn im Stand
-		erstesElement = elemente["Stand"][random.randrange(0, len(elemente))]
-		uebung.append(erstesElement)
+		erstesElementMitFolgeelement = elemente["Stand"][random.randrange(0, len(elemente))]
+		uebung.append(erstesElementMitFolgeelement)
 	elif spruengeInUebung > 0:
-		letztesElement=uebung[-1]
-		erlaubtesElement="nein"
-		while erlaubtesElement=="nein":
-		  zufallselement= list(elemente.keys())[random.randrange(0,len(elemente))]
-		  if letztesElement in elemente[zufallselement]:
-		    erlaubtesElement="ja"
+		letztePosition=uebung[-1][1]
+		zufallselement= elemente[letztePosition][random.randrange(0,len(elemente))]
 		uebung.append(zufallselement)
 	spruengeInUebung += 1
 print("\n\n")
 for sprung in uebung:
-  print (sprung)
+  print (sprung[0])
