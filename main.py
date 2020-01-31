07f91ad4e38bf19c891b1569aa48220a6c012683
Ma173 <malte.gerhardt@rub.de>
2020-01-31 03:16:49 +0000

    Schleife umgeschrieben

--- main.py
+++ main.py
@@ -15,36 +15,36 @@ elemente={
 
   #IN STAND
   "Stand":((0.1,"Rücken","Stand",0),(0.1,"Bauch","Stand",0),(0,"Sitz","Stand",0)),
-  "Hocke":(0,"Stand","Stand",0),
-  "Bücke":(0,"Stand","Stand",0),
-  "Grätsche":(0,"Stand","Stand",0),
-  "halbe Schraube":(0.1,"Stand","Stand",0),
-  "halbe Hocke":(0.1,"Stand","Stand",1),
-  "halbe Bücke":(0.1,"Stand","Stand",1),
-  "halbe Grätsche":(0.1,"Stand","Stand",1),
-  "ganze Schraube":(0.2,"Stand","Stand",0),
+  "Hocke":((0,"Stand","Stand",0)),
+  "Bücke":((0,"Stand","Stand",0)),
+  "Grätsche":((0,"Stand","Stand",0)),
+  "halbe Schraube":((0.1,"Stand","Stand",0)),
+  "halbe Hocke":((0.1,"Stand","Stand",1)),
+  "halbe Bücke":((0.1,"Stand","Stand",1)),
+  "halbe Grätsche":((0.1,"Stand","Stand",1)),
+  "ganze Schraube":((0.2,"Stand","Stand",0)),
   "ganze Stand":((0.2,"Sitz","Stand",1),(0.3,"Rücken","Stand",1)),
-  "Muffel":(0.3,"Rücken","Stand",0),
-  "Salto vorwärts z. Stand":(0.6,"Rücken","Rücken",1),
+  "Muffel":((0.3,"Rücken","Stand",0)),
+  "Salto vorwärts z. Stand":((0.6,"Rücken","Rücken",1)),
 
   #IN SITZ
   "Sitz":((0,"Stand","Stand",0),(0,"Sitz","Sitz")),
   "halbe Sitz":((0.1,"Stand","Sitz",0),(0.1,"Sitz", "Sitz",0),(0.2,"Rücken","Sitz",1)),
   "ganze Sitz":((0.2,"Sitz","Sitz",0),(0.2,"Stand","Sitz",1)),
-  "Hocke in den Sitz":(0,"Stand","Sitz",2),
-  "Grätsche in den Sitz":(0,"Stand","Sitz",2),
-  "Muffel in den Sitz":(0.3,"Rücken","Stand",0),
+  "Hocke in den Sitz":((0,"Stand","Sitz",2)),
+  "Grätsche in den Sitz":((0,"Stand","Sitz",2)),
+  "Muffel in den Sitz":((0.3,"Rücken","Stand",0)),
   
   #IN RÜCKEN
   "Rücken":((0.1,"Stand","Rücken",0),(0.1,"Sitz","Rücken",1),(0,"Rücken","Rücken",0),(0.2,"Bauch","Rücken",0)),
   "Halbe Rücken":((0.2,"Stand","Rücken",0),(0.3,"Rücken","Rücken",0),(0.2,"Sitz","Halbe Rücken",1)),
-  "Salto vorwärts z. Rücken c":(0.5,"Rücken","Rücken"),
-  "Muffel in den Rücken":(0.5,"Rücken","Stand",1),
+  "Salto vorwärts z. Rücken c":((0.5,"Rücken","Rücken")),
+  "Muffel in den Rücken":((0.5,"Rücken","Stand",1)),
   
   #IN BAUCH
   "Bauch":((0.1,"Stand","Bauch",0),(0.1,"Sitz","Bauch",0),(0,"Bauch","Bauch",0),(0.2,"Rücken","Bauch",1)),
-  "halbe Heli":(0.1,"Bauch","Bauch",0),
-  "Muffel in den Bauch":(0.2,"Rücken","Bauch",1)
+  "halbe Heli":((0.1,"Bauch","Bauch",0)),
+  "Muffel in den Bauch":((0.2,"Rücken","Bauch",1))
 
 }
 
@@ -53,10 +53,15 @@ ausSitz=[]
 ausRücken=[]
 ausBauch=[]
 
+#TODO: Schleife so umbauen, dass die Schleife bei Tupeln mit nur einem Element nicht eine Etage tiefer (in dessen vier Einträge) reingeht
 for element in elemente:
+  print("Neues Element:",element,"mit einer Länge von",len(element))
   varianten=elemente[element]
   for variante in varianten:
+    print("Variante ist:",variante)
     startposition=variante[1]
+    #startposition=""
+    print("Startposition ist",startposition)
     if startposition=="Stand":
       ausStand.append(variante)
     elif startposition=="Sitz":
@@ -104,23 +109,42 @@ spruengeInUebung = 0
 # weirdheitsfaktor=users[currentuser][1]
 
 while spruengeInUebung < 10:
-	if spruengeInUebung == 0:
+  if spruengeInUebung == 0:
 	  #Beginn im Stand
-		erstesElement = ausStand[random.randrange(0, len(elemente))]
+    erstesElement = ausStand[random.randrange(0, len(elemente))]
     weirdheitDesElements = erstesElement[3]
     while weirdheitDesElements>erlaubteWeirdheit:
       erstesElement = ausStand[random.randrange(0, len(elemente))]
       weirdheitDesElements = erstesElement[3]
-
-		uebung.append(erstesElement)
-	elif spruengeInUebung > 0:
-		letztePosition=uebung[-1][1]
+      uebung.append(erstesElement)
+  elif spruengeInUebung > 0:
+    letztePosition=uebung[-1][1]
     if letztePosition=="Stand":
-		  zufallselement= ausStand[letztePosition][random.randrange(0,len(elemente))]
-    while weirdheitDesElements>erlaubteWeirdheit:
-
-		uebung.append(zufallselement)
-	spruengeInUebung += 1
+      zufallselement= ausStand[random.randrange(0,len(elemente))]
+      weirdheitDesElements = zufallselement[3]
+      while weirdheitDesElements>erlaubteWeirdheit:
+        zufallselement= ausStand[random.randrange(0,len(elemente))]
+        weirdheitDesElements = zufallselement[3]
+    elif letztePosition=="Sitz":
+      zufallselement= ausSitz[random.randrange(0,len(elemente))]
+      weirdheitDesElements = zufallselement[3]
+      while weirdheitDesElements>erlaubteWeirdheit:
+        zufallselement= ausSausSitztand[random.randrange(0,len(elemente))]
+        weirdheitDesElements = zufallselement[3]
+    elif letztePosition=="Bauch":
+      zufallselement= ausBauch[random.randrange(0,len(elemente))]
+      weirdheitDesElements = zufallselement[3]
+      while weirdheitDesElements>erlaubteWeirdheit:
+        weirdheitDesElements = zufallselement[3]
+        zufallselement= ausBauch[random.randrange(0,len(elemente))]
+    elif letztePosition=="Rücken":
+      zufallselement= ausRücken[random.randrange(0,len(elemente))]
+      weirdheitDesElements = zufallselement[3]
+      while weirdheitDesElements>erlaubteWeirdheit:
+        weirdheitDesElements = zufallselement[3]
+        zufallselement= ausRücken[random.randrange(0,len(elemente))]
+    uebung.append(zufallselement)
+  spruengeInUebung += 1
 print("\n\n")
 for sprung in uebung:
   print (sprung[0])
